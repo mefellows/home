e4becfee94ad73161f2d2cca241561b6e964382c
Matt Fellows <matt.fellows@onegeek.com.au>
2016-12-29 12:58:56 +1100

    wip: fix host/port path in connection string

--- db/db.go
+++ db/db.go
@@ -38,7 +38,7 @@ func GetDatabase(connectionString string) (db *gorm.DB) {
 // Convert Postgres connection string to Gorm string
 func ConnectionStringToGormString(connectionString string) (adapter string, gormString string) {
 
-	r := regexp.MustCompile(`([a-zA-Z]+)\:\/\/([a-zA-Z0-9_]+)\:([a-zA-Z0-9_]+)\@([0-9a-zA-Z_-\.]+)\:([0-9]{4,5})/([a-zA-Z0-9_]+)(.*)`)
+	r := regexp.MustCompile(`([a-zA-Z]+)\:\/\/([a-zA-Z0-9_]+)\:([a-zA-Z0-9_]+)\@([0-9a-zA-Z\_\-\.]+)\:([0-9]{4,5})/([a-zA-Z0-9_]+)(.*)`)
 	matches := r.FindStringSubmatch(connectionString)
 
 	return matches[1], fmt.Sprintf("host=%s user=%s dbname=%s password=%s port=%s sslmode=disable", matches[4], matches[2], matches[6], matches[3], matches[5])
